2766d617ef7e170ec2b7a3c5dcffebe91a5bd55f
JacquelineFM <jfloresm.dev@gmail.com>
2022-10-01 01:02:24 -0500

    feat: add hook to access provider data

---
+++ src/hooks/useQuote.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import QuoteContext from "../constants/QuoteProvider";
+
+/**
+ * UseQuote() is a function that returns the useContext() function, which is passed the QuoteContext
+ * object.
+ * @returns The useContext hook is being used to return the QuoteContext.
+ */
+const useQuote = () => {
+  return useContext(QuoteContext);
+};
+
+export default useQuote;
